a58f34f4ac54f7d56d5a4c27ab464ea25335517d
jualoppaz <juanmalp1992@gmail.com>
2014-07-01 18:38:33 +0200

    Actualizado el archivo email-settings.js
    Eliminada toda mi información privada de este archivo.

--- app/server/modules/email-settings.js
+++ app/server/modules/email-settings.js
@@ -1,14 +1,13 @@
 
 module.exports = {
 	/*
+	
+	Fill this fields with your mail credentials
+	
 	host		: 'smtp.gmail.com',
 	user 		: 'your-email-address@gmail.com',
 	password 	: 'your-email-password',
 	sender		: 'Your Name <your-email-address@gmail.com>'
 	*/
 	
-	host		: 'smtp.gmail.com',
-	user 		: 'juanmalp1992@gmail.com',
-	password 	: 'juanmita14',
-	sender		: 'Juanma <juanmalp1992@gmail.com>'
 }
