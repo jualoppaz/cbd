0a44ed8e0b8f32ab0c8f2b55a2d8962d8040d872
jualoppaz <juanmalp1992@gmail.com>
2014-07-03 19:23:03 +0200

    Aclaracion sobre la variable de entorno en Heroku

--- app/server/modules/email-settings.js
+++ app/server/modules/email-settings.js
@@ -3,12 +3,14 @@ module.exports = {
 	
 	host		: 'smtp.gmail.com',
 	user 		: 'juanmalp1992@gmail.com',
-	password 	: 'juanmita14',
+	password 	: process.env.EMAIL_PASSWORD,
 	sender		: 'Juanma <juanmalp1992@gmail.com>'
 
     /*
 
     You must fill this fields with your mail credentials but the "host" field.
 
-     */
+    The "password" field is a Heroku var. If you try this app in localhost it won't work by anyway.
+
+    */
 }
